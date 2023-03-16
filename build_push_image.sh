a2b9cc550897d5c676f1f0509e78b53e1f31c5fc
ramafebri-aladin <rama.febriansyah@misteraladin.com>
2023-03-16 14:38:01 +0700

    commit file submission

---
+++ build_push_image.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Proyek pertama - ramafebri
+
+docker build -t item-app:v1 .
+docker logout
+export PASSWORD_DOCKER_HUB=rama260299
+echo $PASSWORD_DOCKER_HUB | docker login -u ramafebri --password-stdin
+docker tag item-app:v1 ramafebri/item-app:v1
+docker push ramafebri/item-app:v1
