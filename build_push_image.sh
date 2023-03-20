5e780ce5c9513f428a3cfda9631f1bdfa4856cfc
ramafebri-aladin <rama.febriansyah@misteraladin.com>
2023-03-20 09:35:03 +0700

    update build push image file

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,8 +2,9 @@
 # Proyek pertama - ramafebri
 
 docker build -t item-app:v1 .
+docker images
+docker tag item-app:v1 ramafebri/item-app:v1
 docker logout
 export PASSWORD_DOCKER_HUB=rama260299
 echo $PASSWORD_DOCKER_HUB | docker login -u ramafebri --password-stdin
-docker tag item-app:v1 ramafebri/item-app:v1
 docker push ramafebri/item-app:v1
